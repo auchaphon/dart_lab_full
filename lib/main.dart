6c4ca3f11cfe176ad46a0a23a5f2990fc77e21ab
auchaphon phungsema <auchaphon@gmail.com>
2020-05-13 11:05:54 +0700

    finit

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,26 @@
 import 'package:flutter/material.dart';
-import './widget/transaction.dart';
+
+import './widgets/user_transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      home: MyHomePage(),
-    );
+    return MaterialApp(title: 'Flutter App', home: MyHomePage());
   }
 }
 
+
+
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1',
-        title: 'Pay for Krapao Kai',
-        amount: 200,
-        date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Buy the new notebook',
-        amount: 20000,
-        date: DateTime.now()),
-  ];
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Flutter")),
-        body: Container(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: transactions.map((tx) {
-                return Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(
-                          Icons.tv,
-                          size: 40,
-                          color: Colors.blue,
-                        ),
-                        title: Text("${tx.id} - ${tx.title}"),
-                        subtitle: Text("${tx.title} - ${tx.amount}"),
-                      ),
-                    ],
-                  ),
-                );
-              }).toList()),
-          height: double.infinity,
-        ));
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: SingleChildScrollView(
+        child: UserTransactions(),
+      ),
+    );
   }
 }
